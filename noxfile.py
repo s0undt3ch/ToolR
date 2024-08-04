83ae96056249187bca0c4d4803c099b9556a0b0c
Pedro Aalgarvio <pedro@algarvio.me>
2024-08-04 19:30:34 +0100

    It's Alive!
    https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXE0cjE1dHNtajJscmV5cWNzdXczMmtpbGtwbWg1NzRodWhpdWpqMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1SRaXI2J1o7vO/giphy.gif

--- noxfile.py
+++ noxfile.py
@@ -10,8 +10,15 @@ os.chdir(str(REPO_ROOT))
 
 @nox.session()
 def test(session: nox.Session):
-    session.install(str(REPO_ROOT), "wheel")
+    session.install("wheel")
+    session.install("-e", str(REPO_ROOT), "wheel")
     # Ensure build uses version of setuptools-rust under development
-    session.install("--no-build-isolation", ".[dev]")
+    session.install(
+        "--no-build-isolation",
+        "-r",
+        str(REPO_ROOT / "requirements.txt"),
+        "-r",
+        str(REPO_ROOT / "requirements-dev.txt"),
+    )
     # Test Python package
     session.run("pytest", *session.posargs)
