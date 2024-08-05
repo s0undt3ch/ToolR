081a3edc8b176b2d7667950fc5a67ac6c8ef9cf1
Pedro Aalgarvio <pedro@algarvio.me>
2024-08-05 20:12:51 +0100

    Plain python for now

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,10 +1,5 @@
-import toolr
-import toolr.rust  # type: ignore[import-not-found]
+import toolr  # type: ignore[import-not-found]
 
 
 def test_python():
     assert toolr.python_func() == 15
-
-
-def test_rust():
-    assert toolr.rust.rust_func() == 14
