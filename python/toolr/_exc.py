f96fd1befcbd8b2b4b11d1de7272833e61aa98bf
Pedro Algarvio <pedro@algarvio.me>
2025-08-25 07:14:54 +0100

    fix(SignatureError): `SignatureError` exceptions now point to command
    Now, all raised ``SignatureError`` exception messages get prefixed with the
    fully qualified function name:
    
    ```
    Traceback (most recent call last):
      File ".../python/toolr/utils/_signature.py", line 251, in get_signature
        parameter = _parse_parameter(param_name, param, resolved_annotation, parsed_docstring)
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File ".../python/toolr/utils/_signature.py", line 383, in _parse_parameter
        raise SignatureParameterError(err_msg)
    toolr._exc.SignatureParameterError: Arg 'version' has no description in the docstring which is required to generate the help message.
    
    The above exception was the direct cause of the following exception:
    
    Traceback (most recent call last):
      File ".../.venv/bin/toolr", line 10, in <module>
        sys.exit(main())
                 ^^^^^^
      File ".../python/toolr/__main__.py", line 37, in main
        registry.discover_and_build(parser)
      File ".../python/toolr/_registry.py", line 234, in discover_and_build
        self._build_parsers()
      File ".../python/toolr/_registry.py", line 218, in _build_parsers
        signature = get_signature(commands[command_name])
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File ".../python/toolr/utils/_signature.py", line 253, in get_signature
        raise SignatureError(exc.message, func) from exc
    toolr._exc.SignatureError: tools.version.commit: Arg 'version' has no description in the docstring which is required to generate the help message.
    ```
    
    Fixes #27

--- python/toolr/_exc.py
+++ python/toolr/_exc.py
@@ -1,9 +1,24 @@
 from __future__ import annotations
 
+from collections.abc import Callable
+
 
 class ToolrError(Exception):
     """Base exception for all Toolr errors."""
 
 
-class SignatureError(ToolrError):
+class SignatureParameterError(ToolrError):
+    """Exception raised when a function signature parameter is invalid."""
+
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(message)
+
+
+class SignatureError(SignatureParameterError):
     """Exception raised when a function signature is invalid."""
+
+    def __init__(self, message: str, func: Callable | None = None) -> None:
+        if func is not None:
+            message = f"{func.__module__}.{func.__name__}: {message}"
+        super().__init__(message)
